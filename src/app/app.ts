807bbc0e2e9047df23eb332528c1d0ba94015640
RealApexMan <realapexman@gmail.com>
2025-11-20 13:56:51 +0100

    Add Header feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarSmartComponent } from "./core/navbar/navbar.smart.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarSmartComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
