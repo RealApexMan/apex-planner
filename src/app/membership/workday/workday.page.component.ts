4b971121182c840cdcd44eeb2f4eedddd12ee269
RealApexMan <realapexman@gmail.com>
2025-11-23 17:56:49 +0100

    Refactoring; transfer task field into a dumb component (#21)

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TaskFieldDumbComponent } from './task-field/task-field.dumb.component';
 import { WorkdayStore } from './workday.page.store';
 
 @Component({
   selector: 'app-workday',
-  imports: [],
+  imports: [TaskFieldDumbComponent],
   templateUrl: './workday.page.component.html',
   styleUrl: './workday.page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
