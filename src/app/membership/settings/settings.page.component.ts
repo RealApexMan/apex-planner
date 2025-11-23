f55780ef5a6d8c0163f803554fd5bd1ba8f72316
RealApexMan <realapexman@gmail.com>
2025-11-23 14:31:22 +0100

    Create Workday view model (#18)

--- src/app/membership/settings/settings.page.component.ts
+++ src/app/membership/settings/settings.page.component.ts
@@ -5,8 +5,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './settings.page.component.html',
   styleUrl: './settings.page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsPageComponent {
-
-}
+export class SettingsPageComponent {}
