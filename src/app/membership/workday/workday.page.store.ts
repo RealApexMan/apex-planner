4232004053f02d988348bb85d3a3982b0dc91924
RealApexMan <realapexman@gmail.com>
2025-12-20 19:08:10 +0100

    Domain (#31)

--- src/app/membership/workday/workday.page.store.ts
+++ src/app/membership/workday/workday.page.store.ts
@@ -1,5 +1,6 @@
 import { computed, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Workday } from '@app/core/entity/workday';
 import {
   patchState,
   signalStore,
@@ -22,64 +23,39 @@ import {
 } from './task.model';
 
 interface WorkdayState {
-  date: string;
-  taskList: TaskList;
+  workday: Workday;
   progress: number;
-  mode: 'edit' | 'execution';
 }
-
-const getEmptyTask = (): Task => ({
-  type: 'Hit the target',
-  title: 'Nouvelle tâche',
-  status: 'Not started',
-  pomodoroCount: 1,
-  pomodoroList: [0],
-  statusEmoji: '🏁',
-});
-
-const WORKDAY_TASK_LIMIT = 6;
 
 export const WorkdayStore = signalStore(
   withState<WorkdayState>({
-    date: '2019-02-28',
-    taskList: [getEmptyTask()],
+    workday: Workday.createEmpty(),
     progress: 0,
-    mode: 'edit',
   }),
   withProps(() => ({
     destroyRef: inject(DestroyRef),
+    /*
+     * Using Subject for intra-store event.
+     */
     pomodoroCompleted: new Subject<void>(),
   })),
   withComputed((state) => {
-    const taskCount = computed(() => state.taskList().length);
-    const isButtonDisplayed = computed(() => taskCount() < WORKDAY_TASK_LIMIT);
-    const hasNoTaskPlanned = computed(() => taskCount() === 0);
-    const hasTaskPlanned = computed(() => taskCount() > 0);
-    const isEditMode = computed(() => state.mode() === 'edit');
-    const isExecutionMode = computed(() => state.mode() === 'execution');
     const pomodoroProgress = computed(() => {
       return Math.floor((state.progress() / MAXIMUM_POMODORO_DURATION) * 100);
     });
 
     return {
-      taskCount,
-      isButtonDisplayed,
-      hasNoTaskPlanned,
-      hasTaskPlanned,
-      isEditMode,
-      isExecutionMode,
       pomodoroProgress,
     };
   }),
   withMethods(({ destroyRef, pomodoroCompleted, ...store }) => ({
     startWorkday() {
-      patchState(store, { mode: 'execution' });
-      console.log('Workday started!');
+      patchState(store, ({ workday }) => ({
+        workday: workday.setExecutionMode(),
+      }));
       timer(0, 1000)
         .pipe(takeUntil(pomodoroCompleted), takeUntilDestroyed(destroyRef))
         .subscribe((elapsedSeconds: number) => {
-          console.log('elapsedSeconds', elapsedSeconds);
-
           patchState(store, { progress: elapsedSeconds });
           patchState(store, (state) => {
             // Update current pomodoro time immutably so signals detect the change
@@ -111,43 +87,36 @@ export const WorkdayStore = signalStore(
 
             const taskList: TaskList = store.taskList().toSpliced(taskIndex, 1, updatedTask);
 
-            return { taskList };
+          patchState(store, ({ workday }) => {
+            return { workday: workday.tick() };
           });
-
-          // Check completed state
-          if (elapsedSeconds === MAXIMUM_POMODORO_DURATION) {
-            pomodoroCompleted.next();
-            patchState(store, { mode: 'edit', progress: 0 });
-          }
         });
     },
-    isWorkdayCompleted(): boolean {
-      return store.taskList().every((task) => {
-        return isTaskCompleted(task);
-      });
-    },
-    isTaskCompleted(task: Task): boolean {
-      return isTaskCompleted(task);
-    },
     addTask() {
-      patchState(store, (state) => ({
-        taskList: [...state.taskList, getEmptyTask()],
+      patchState(store, ({ workday }) => ({
+        workday: workday.addEmptyTask(),
       }));
     },
-    removeTask($index: number) {
-      patchState(store, (state) => ({
-        taskList: state.taskList.toSpliced($index, 1),
+    removeTask(index: number) {
+      patchState(store, ({ workday }) => ({
+        workday: workday.removeTask(index),
       }));
     },
     updateDate(event: Event) {
       const date = (event.target as HTMLInputElement).value;
-      patchState(store, () => ({ date }));
+      patchState(store, ({ workday }) => ({
+        workday: workday.createEmptyAtDate(date),
+      }));
     },
     updateTask(index: number, updatedTask: Task) {
-      patchState(store, (state) => {
-        const taskList: TaskList = state.taskList.toSpliced(index, 1, updatedTask);
-        return { taskList };
-      });
+      patchState(store, ({ workday }) => ({
+        workday: workday.updateTask(index, updatedTask),
+      }));
     },
+    // TODO: Persist current Workday on Firestore.
+    // saveWorkday(workday: Workday): Observable<void> {
+    //   console.log('Workday saved!', workday);
+    //   return of(undefined);
+    // },
   }))
 );
